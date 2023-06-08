9ae5d73b9588a992e8dd42104cbb166c9bc672d9
madhu <kunchapumadhu96@gmail.com>
2023-06-08 13:02:56 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = request("json-server");  //import json-server library
 const server = jsonServer.create();
 const router =jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port =process.env.PORT || 3004; // chose port from here like 8080, 3002
+const port =process.env.PORT || 8080; // chose port from here like 8080, 3002
 
 server.use(middlewares);
 server.use(router);
